82daf783170ec0a0e687b2d0ab948761bf8a7753
Andreev Alexander <andreevlex.as@gmail.com>
2017-08-20 20:02:15 +0300

    Добавлена функция check_digit

--- src/inn.rs
+++ src/inn.rs
@@ -8,9 +8,7 @@ pub fn is_valid_inn(input: &str) -> bool {
         return false;
     }
 
-    let len = input.len();
-    
-    match len {
+    match input.len() {
         12 => check_inn_len12(input),
         10 => check_inn_len10(input),
         _ => false
@@ -42,40 +40,30 @@ fn get_digit(input: &str, n: usize) -> u32 {
     }
 }
 
-fn check_inn_len12(input: &str) -> bool {
-    let ratio1:[u32; 10] = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
-    let mut sum1 = 0;
-    
-    for i in 0..ratio1.len() {
-        let num = get_digit(input, i);
-        sum1 += num * ratio1[i];
-    }
-    let calc_num1 = sum1 % 11 % 10;
-    
-    let ratio2:[u32; 11] = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8];    
-    let mut sum2 = 0;    
-    for i in 0..ratio2.len() {
-        let num = get_digit(input, i);
-        sum2 += num * ratio2[i];
-    }
-    let calc_num2 = sum2 % 11 % 10;
-    
-    let ctrl_num1 = get_digit(input, 10);
-    let ctrl_num2 = get_digit(input, 11);
-
-    calc_num1 == ctrl_num1 && calc_num2 == ctrl_num2
-}
-
-fn check_inn_len10(input: &str) -> bool {
-    let ratio:[u32; 9] = [2, 4, 10, 3, 5, 9, 4, 6, 8];
+fn check_digit(input: &str, ratio: &[u32]) -> u32 {
     let mut sum = 0;
     
     for i in 0..ratio.len() {
         let num = get_digit(input, i);
         sum += num * ratio[i];
     }
-    let calc_num = sum % 11 % 10;
-    let ctrl_num = get_digit(input, 9);
+    sum % 11 % 10
+}
 
-    calc_num == ctrl_num
+fn check_inn_len12(input: &str) -> bool {
+    let ratio1:[u32; 10] = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
+    let calc_num1 = check_digit(input, &ratio1);
+    
+    let ratio2:[u32; 11] = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8];    
+    let calc_num2 = check_digit(input, &ratio2);
+    
+    calc_num1 == get_digit(input, 10) && calc_num2 == get_digit(input, 11)
+}
+
+fn check_inn_len10(input: &str) -> bool {
+    let ratio:[u32; 9] = [2, 4, 10, 3, 5, 9, 4, 6, 8];
+    
+    let calc_num = check_digit(input, &ratio);
+    
+    calc_num == get_digit(input, 9)
 }
