0ab618d02f9b932417c728be3aa91892fc9ec194
Alexander Andreev <andreevlex.as@gmail.com>
2018-01-13 17:48:57 +0300

    remove fns_service

--- src/fns_service/nds_response.rs
+++
@@ -1,8 +0,0 @@
-use chrono::prelude::*;
-
-#[derive(Debug)]
-pub struct NdsResponse<'a> {
-    pub dtact_fl: DateTime<Utc>,
-    pub dtact_ul: DateTime<Utc>,
-    pub partners: Vec<super::Partner<'a>>,
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,7 @@
-extern crate chrono;
-#[macro_use]
-extern crate hyper;
-#[macro_use]
-extern crate log;
 extern crate regex;
-extern crate reqwest;
-extern crate xml;
-extern crate xmltree;
 
 pub mod checkers;
 pub mod error;
-pub mod fns_service;
 #[cfg(test)]
 mod unit_test;
 
